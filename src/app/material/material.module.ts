6b1a9ba77189b866ec439f7fb08501d7559647ee
beataj <beata@emc2wave.com>
2022-01-24 00:25:11 -0800

    start project

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+const material = [MatButtonModule];
+
+@NgModule({
+  declarations: [],
+  imports: [material],
+  exports: [material]
+})
+export class MaterialModule { }
