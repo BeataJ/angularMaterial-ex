a5d4446ab63b233ae74fedd307efa7920ad280d1
beataj <beata@emc2wave.com>
2022-01-24 16:11:39 -0800

    add amount column

--- src/app/data-table/data-table-datasource.ts
+++ src/app/data-table/data-table-datasource.ts
@@ -8,30 +8,16 @@ import { Observable, of as observableOf, merge } from 'rxjs';
 export interface DataTableItem {
   name: string;
   id: number;
+  amount: number
 }
 
 // TODO: replace this with real data from your application
 const EXAMPLE_DATA: DataTableItem[] = [
-  {id: 1, name: 'Hydrogen'},
-  {id: 2, name: 'Helium'},
-  {id: 3, name: 'Lithium'},
-  {id: 4, name: 'Beryllium'},
-  {id: 5, name: 'Boron'},
-  {id: 6, name: 'Carbon'},
-  {id: 7, name: 'Nitrogen'},
-  {id: 8, name: 'Oxygen'},
-  {id: 9, name: 'Fluorine'},
-  {id: 10, name: 'Neon'},
-  {id: 11, name: 'Sodium'},
-  {id: 12, name: 'Magnesium'},
-  {id: 13, name: 'Aluminum'},
-  {id: 14, name: 'Silicon'},
-  {id: 15, name: 'Phosphorus'},
-  {id: 16, name: 'Sulfur'},
-  {id: 17, name: 'Chlorine'},
-  {id: 18, name: 'Argon'},
-  {id: 19, name: 'Potassium'},
-  {id: 20, name: 'Calcium'},
+  { id: 1, name: 'Hydrogen', amount: 10 },
+  { id: 2, name: 'Helium', amount: 11 },
+  { id: 3, name: 'Lithium', amount: 112 },
+  { id: 4, name: 'Beryllium', amount: 19 },
+  { id: 5, name: 'Boron', amount: 18 },
 ];
 
 /**
